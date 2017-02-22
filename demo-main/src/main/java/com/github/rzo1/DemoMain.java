bbb0bf0a8e02bc247ebb542686551e6cd3f5a7eb
rzo1 <richard@zowalla.com>
2017-02-22 12:24:42 +0100

    starts via ApplicationComposer, but this is not really a CLI use-case?

--- demo-main/src/main/java/com/github/rzo1/DemoMain.java
+++ demo-main/src/main/java/com/github/rzo1/DemoMain.java
@@ -1,34 +1,67 @@
-/**
- * Copyright (c) 2014-2017 Martin Wiesner, Richard Zowalla, Daniel Zsebedits
- * ALL RIGHTS RESERVED
- * <p>
- * Urheber i.S.d. §7 UrhG sind
- * Martin Wiesner, Richard Zowalla, Daniel Zsebedits
- */
 package com.github.rzo1;
 
 import com.github.rzo1.domain.DemoObject;
 import com.github.rzo1.service.DemoService;
-import org.apache.openejb.OpenEjbContainer;
+import com.github.rzo1.service.DemoServiceImpl;
+import org.apache.openejb.config.EjbModule;
+import org.apache.openejb.jee.EjbJar;
+import org.apache.openejb.jee.StatelessBean;
+import org.apache.openejb.jee.jpa.unit.PersistenceUnit;
+import org.apache.openejb.testing.ApplicationComposers;
+import org.apache.openejb.testing.Classes;
+import org.apache.openejb.testing.Configuration;
+import org.apache.openejb.testing.Module;
 
-import javax.ejb.embeddable.EJBContainer;
+import javax.ejb.Stateless;
 import javax.inject.Inject;
+import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Properties;
-import java.util.concurrent.CountDownLatch;
 
-public class DemoMain implements Runnable {
+@Stateless
+public class DemoMain    {
 
     private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(DemoMain.class);
-
-    private static final String applicationName = "DemoMain";
-
-    private boolean ejbContainerReady;
 
     @Inject
     private DemoService demoService;
 
+    @Module
+    @Classes({ DemoServiceImpl.class })
+    public EjbModule application() {
+        final EjbJar ejbJar = new EjbJar().enterpriseBean(new StatelessBean(DemoMain.class));
+        ejbJar.addEnterpriseBean(new StatelessBean(DemoServiceImpl.class));
+
+        final EjbModule ejbModule = new EjbModule(ejbJar);
+
+        ejbModule.setModuleId("batchee-shared-components");
+        return ejbModule;
+    }
+
+    @Module
+    public PersistenceUnit persistence() {
+        PersistenceUnit unit = new PersistenceUnit("demo");
+        unit.setJtaDataSource("demo");
+        unit.setNonJtaDataSource("demoJTA");
+        unit.getClazz().add(DemoObject.class.getName());
+        return unit;
+    }
+
+    @Configuration
+    public Properties configuration() throws URISyntaxException {
+        final Properties properties = new Properties();
+        properties.setProperty("openejb.jaxrs.application", "false");
+
+        Path launchPath = Paths.get(DemoMain.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+        properties.setProperty("openejb.configuration", launchPath.toAbsolutePath() + "/conf/openejb.xml");
+
+        properties.put("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+        properties.put("hibernate.connection.driver_class", "org.hsqldb.jdbcDriver");
+
+
+        return properties;
+    }
     /**
      * @param args containing  the project oid and the paths to the csv files to parse. First argument specifies the
      *             eventCSV file, second
@@ -38,27 +71,9 @@ public class DemoMain implements Runnable {
     public static void main(String[] args) throws Exception {
 
         logger.info("Application container start...");
-        DemoMain importer;
         try {
-            importer = new DemoMain();
-            final Thread thread = new Thread(importer);
-            thread.start();
 
-            // looking of openejb container to come up
-            while (!importer.ejbContainerReady) {
-                Thread.sleep(200);
-            }
-            logger.info("Application container start... [OK]");
-
-            DemoObject demoObject = new DemoObject();
-
-            long oid = importer.demoService.store(demoObject);
-
-            demoObject = importer.demoService.get(oid);
-
-            logger.info("Object: {}", demoObject.toString());
-
-            System.exit(0);
+            ApplicationComposers.run(DemoMain.class, args);
 
 
         } catch (final Exception e) {
@@ -67,59 +82,5 @@ public class DemoMain implements Runnable {
         }
     }
 
-    @Override
-    public void run() {
-        EJBContainer ejbContainer = null;
-        try {
-            final Properties properties = new Properties();
-            properties.setProperty(EJBContainer.APP_NAME, applicationName);
-            properties.setProperty(EJBContainer.PROVIDER, OpenEjbContainer.class.getName());
-            properties.setProperty(OpenEjbContainer.OPENEJB_EMBEDDED_REMOTABLE, "false");
-            properties.setProperty("ejbd.disabled", "true");
-            properties.setProperty("ejbds.disabled", "true");
-            properties.setProperty("admin.disabled", "true");
-            properties.setProperty("openejb.jaxrs.application", "false");
-
-            Path launchPath = Paths.get(DemoMain.class.getProtectionDomain().getCodeSource().getLocation().toURI());
-            properties.setProperty("openejb.configuration", launchPath.toAbsolutePath() + "/conf/openejb.xml");
-
-            properties.put("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
-            properties.put("hibernate.connection.driver_class", "org.hsqldb.jdbcDriver");
-
-            // This is the line starting the EJB container
-            ejbContainer = EJBContainer.createEJBContainer(properties);
-            ejbContainer.getContext().bind("inject", this);
-
-            ejbContainerReady = true;
-
-            final CountDownLatch latch = new CountDownLatch(1);
-            // Graceful shutdown
-            Runtime.getRuntime().addShutdownHook(new Thread() {
-                @Override
-                public void run() {
-                    try {
-                        logger.info("Shutting down..");
-                        latch.countDown();
-                        logger.info("Shutdown completed successfully.");
-                    } catch (final Exception e) {
-                        logger.error("Graceful shutdown went wrong. SIGKILL (kill -9) if you want.", e);
-                    }
-                }
-            });
-            try {
-                latch.await();
-            } catch (final InterruptedException e) {
-                // ignored
-            }
-        } catch (final Exception e) {
-            ejbContainerReady = false;
-            throw new RuntimeException(e);
-        } finally {
-            if (ejbContainer != null) {
-                ejbContainer.close();
-            }
-        }
-
-    }
 
 }
